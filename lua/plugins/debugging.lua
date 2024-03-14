b5907bef4b1c10e6aed2cd3e24308a2c46f73576
Raymond Selvik <raymond.d.selvik@gmail.com>
2024-03-14 21:48:16 +0100

    debugger

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,63 +1,80 @@
 return {
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-            "mfussenegger/nvim-dap-python"
-		},
-		config = function()
-			local dap = require("dap")
-			local dapui = require("dapui")
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "mfussenegger/nvim-dap-python",
+        },
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
             local dap_python = require("dap-python")
-			dapui.setup()
+            dapui.setup()
 
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
 
             dap_python.setup(vim.fn.getcwd() .. "/.venv/bin/python")
 
-			dap.adapters.codelldb = {
-				type = "server",
-				port = "${port}",
-				executable = {
-					-- CHANGE THIS to your path!!printenv | grep MYVIMRC
-					command = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/adapter/codelldb",
-					args = { "--port", "${port}" },
+            dap.adapters.codelldb = {
+                type = "server",
+                port = "${port}",
+                executable = {
+                    -- CHANGE THIS to your path!!printenv | grep MYVIMRC
+                    command = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/adapter/codelldb",
+                    args = { "--port", "${port}" },
 
-					-- On windows you may have to uncomment this:
-					-- detached = false,
-				},
-			}
+                    -- On windows you may have to uncomment this:
+                    -- detached = false,
+                },
+            }
 
-			dap.configurations.cpp = {
-				{
-					name = "Launch file",
-					type = "codelldb",
-					request = "launch",
-					program = function()
-						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-					end,
-					cwd = "${workspaceFolder}",
-					stopOnEntry = false,
-				},
-			}
+            dap.configurations.cpp = {
+                {
+                    name = "Launch file",
+                    type = "codelldb",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                    end,
+                    cwd = "${workspaceFolder}",
+                    stopOnEntry = false,
+                },
+            }
 
-			dap.configurations.c = dap.configurations.cpp
-			dap.configurations.rust = dap.configurations.cpp
+            dap.configurations.c = dap.configurations.cpp
+            dap.configurations.rust = dap.configurations.cpp
 
-			vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
-			vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-		end,
-	},
-	{},
+            dap.adapters.coreclr = {
+                type = "executable",
+                command = vim.env.HOME .. "/dev/netcoredbg/netcoredbg-3.0.0-1018/netcoredbg",
+                args = { "--interpreter=vscode" },
+            }
+
+            dap.configurations.cs = {
+                {
+                    type = "coreclr",
+                    name = "launch - netcoredbg",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+                    end,
+                },
+            }
+
+            vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
+            vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+        end,
+    },
+    {},
 }
